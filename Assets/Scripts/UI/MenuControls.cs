f046fc07132e5a5168aba2186c85ced5e833cf94
David Weatherhead <dlweatherhead@gmail.com>
2021-01-31 14:43:15 +0100

    Added menu controls to game.

---
+++ Assets/Scripts/UI/MenuControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            QuitGame();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MainMenu();
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
